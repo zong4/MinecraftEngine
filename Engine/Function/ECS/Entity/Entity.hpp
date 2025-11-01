82e87df78b70fe447b95cf0ef7ec3d6105915bb2
zong4 <zzoonng@163.com>
2025-11-01 13:23:18 +0100

    fix entity

--- Engine/Function/ECS/Entity/Entity.hpp
+++ Engine/Function/ECS/Entity/Entity.hpp
@@ -9,21 +9,24 @@ class Entity
 {
 public:
     Entity() = default;
-    Entity(entt::entity handle, entt::registry *registry) : m_Entity(handle), m_Registry(registry) {}
-    virtual ~Entity() { m_Registry = nullptr; }
+    Entity(entt::entity handle, entt::registry *registry) : m_Handle(handle), m_Registry(registry) {}
+    virtual ~Entity()
+    {
+        if (*this)
+            m_Registry->destroy(m_Handle);
+        m_Registry = nullptr;
+    }
 
     // Operators
-    operator bool() const { return m_Entity != entt::null && m_Registry && m_Registry->valid(m_Entity); }
-    operator uint32_t() const { return (uint32_t)m_Entity; }
-    operator entt::entity() const { return m_Entity; }
-    bool operator==(const Entity &other) const { return m_Entity == other.m_Entity; }
-    bool operator!=(const Entity &other) const { return !(*this == other); }
+    operator bool() const { return m_Handle != entt::null && m_Registry && m_Registry->valid(m_Handle); }
+    operator uint32_t() const { return (uint32_t)m_Handle; }
+    operator entt::entity() const { return m_Handle; }
+    bool operator==(const Entity &other) const { return m_Handle == other.m_Handle && m_Registry == other.m_Registry; }
+    bool operator!=(const Entity &other) const { return m_Handle != other.m_Handle || m_Registry != other.m_Registry; }
 
     // Getters
-    // bool IsValid() const { return m_Entity != entt::null && m_Registry && m_Registry->valid(m_Entity); }
-    entt::entity GetHandle() const { return m_Entity; }
+    entt::entity GetHandle() const { return m_Handle; }
     entt::registry &GetRegistry() const { return *m_Registry; }
-    void SetHandle(entt::entity handle) { m_Entity = handle; }
 
 public:
     // clang-format off
@@ -31,36 +34,41 @@ public:
     // clang-format on
     bool HasComponent() const
     {
-        if (!m_Registry || m_Entity == entt::null || !m_Registry->valid(m_Entity))
+        if (!(*this))
             return false;
-        return m_Registry->all_of<T>(m_Entity);
+        return m_Registry->all_of<T>(m_Handle);
     }
+
     // clang-format off
     template <typename T>
     // clang-format on
     T &GetComponent() const
     {
-        if (HasComponent<T>() == false)
-            LOG_ENGINE_INFO("Entity does not have component!");
-        return m_Registry->get<T>(m_Entity);
+        if (!HasComponent<T>())
+            LOG_ENGINE_ERROR("Entity does not have component!");
+        return m_Registry->get<T>(m_Handle);
     }
+
     // clang-format off
     template <typename T, typename... Args>
     // clang-format on
     T &AddComponent(Args &&...args)
     {
-        return m_Registry->emplace<T>(m_Entity, std::forward<Args>(args)...);
+        if (HasComponent<T>())
+            LOG_ENGINE_ERROR("Entity already has component!");
+        return m_Registry->emplace<T>(m_Handle, std::forward<Args>(args)...);
     }
+
     // clang-format off
     template <typename T>
     // clang-format on
     void RemoveComponent()
     {
-        m_Registry->remove<T>(m_Entity);
+        m_Registry->remove<T>(m_Handle);
     }
 
 protected:
-    entt::entity m_Entity = entt::null;
+    entt::entity m_Handle = entt::null;
     entt::registry *m_Registry = nullptr;
 };
 
