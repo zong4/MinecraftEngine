6592b58d2a47a2a754584f37678cbc807c2157b8
zong4 <zzoonng@163.com>
2025-11-21 22:22:10 +0100

    add opengl texture2d

--- Engine/Platform/src/Renderers/Texture/Texture2D.cpp
+++ Engine/Platform/src/Renderers/Texture/Texture2D.cpp
@@ -1,176 +1,68 @@
 #include "Texture2D.hpp"
 
+#include "../RendererProperty.hpp"
+#include "OpenGLTexture2D.hpp"
 #include <glad/glad.h>
 
-Engine::Texture2D::Texture2D(int width, int height, unsigned int internalFormat, unsigned int format, unsigned int type,
-                             void *data)
-    : Texture(), m_Width(width), m_Height(height), m_InternalFormat(internalFormat), m_Format(format), m_Type(type),
-      m_Samples(0)
+std::shared_ptr<Engine::Texture2D> Engine::Texture2D::Create(int width, int height, unsigned int internalFormat,
+                                                             unsigned int format, unsigned int type, void *data)
 {
-    PROFILE_FUNCTION();
-
-    CreateTexture(width, height, internalFormat, format, type, data);
-
-    // Set texture parameters
-    if (m_Format == GL_DEPTH_COMPONENT)
+    switch (RendererProperty::GetInstance().GetAPI())
     {
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
-        float borderColor[] = {1.0f, 1.0f, 1.0f, 1.0f};
-        glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+    case RendererAPI::OpenGL:
+        return std::make_shared<Engine::OpenGLTexture2D>(width, height, internalFormat, format, type, data);
+    case RendererAPI::Vulkan:
+        LOG_ENGINE_ASSERT("Vulkan Texture2D is not implemented yet");
+        return nullptr;
+    default:
+        LOG_ENGINE_ASSERT("Unknown RendererAPI");
+        return nullptr;
     }
-    else if (m_Format == GL_RGBA)
-    {
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    }
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
-
-    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
-Engine::Texture2D::Texture2D(int width, int height, int samples)
-    : Texture(), m_Width(width), m_Height(height), m_InternalFormat(GL_RGBA8), m_Format(GL_RGBA),
-      m_Type(GL_UNSIGNED_BYTE), m_Samples(samples)
+std::shared_ptr<Engine::Texture2D> Engine::Texture2D::Create(int width, int height, int samples)
 {
-    PROFILE_FUNCTION();
-
-    glGenTextures(1, &m_RendererID);
-    glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, m_RendererID);
-
-    glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, samples, m_InternalFormat, width, height, GL_TRUE);
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
-
-    glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
+    switch (RendererProperty::GetInstance().GetAPI())
+    {
+    case RendererAPI::OpenGL:
+        return std::make_shared<Engine::OpenGLTexture2D>(width, height, samples);
+    case RendererAPI::Vulkan:
+        LOG_ENGINE_ASSERT("Vulkan Texture2D is not implemented yet");
+        return nullptr;
+    default:
+        LOG_ENGINE_ASSERT("Unknown RendererAPI");
+        return nullptr;
+    }
 }
 
-Engine::Texture2D::Texture2D(const std::string &path) : Texture(), m_Type(GL_UNSIGNED_BYTE), m_Samples(0)
+std::shared_ptr<Engine::Texture2D> Engine::Texture2D::Create(const std::string &path)
 {
-    PROFILE_FUNCTION();
-
-    glGenTextures(1, &m_RendererID);
-    glBindTexture(GL_TEXTURE_2D, m_RendererID);
-
-    // Load image
-    bool isHDR = false;
+    switch (RendererProperty::GetInstance().GetAPI())
     {
-        int channels = 0;
-        unsigned int type = 0;
-        void *data =
-            LoadImage(path, m_Width, m_Height, channels, m_InternalFormat, m_Format, type, isHDR, /*flip*/ true);
-
-        m_Type = type;
-
-        // Set pixel alignment: HDR textures (float) need 4-byte alignment, LDR can use 1-byte
-        glPixelStorei(GL_UNPACK_ALIGNMENT, isHDR ? 4 : 1);
-        glTexImage2D(GL_TEXTURE_2D, 0, m_InternalFormat, m_Width, m_Height, 0, m_Format, m_Type, data);
-
-        // Generate mipmap only for LDR textures
-        // HDR textures (especially environment maps) typically don't need mipmaps
-        // and some OpenGL implementations may not support mipmap generation for float textures
-        if (!isHDR)
-        {
-            glGenerateMipmap(GL_TEXTURE_2D);
-        }
-
-        RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
-        FreeImage(data);
+    case RendererAPI::OpenGL:
+        return std::make_shared<Engine::OpenGLTexture2D>(path);
+    case RendererAPI::Vulkan:
+        LOG_ENGINE_ASSERT("Vulkan Texture2D is not implemented yet");
+        return nullptr;
+    default:
+        LOG_ENGINE_ASSERT("Unknown RendererAPI");
+        return nullptr;
     }
-
-    // Set texture parameters based on HDR/LDR
-    if (isHDR)
-    {
-        // HDR textures: use linear filtering without mipmap
-        // For environment maps, use CLAMP_TO_EDGE to avoid seams
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    }
-    else
-    {
-        // LDR textures: use mipmap filtering
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    }
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
-
-    glBindTexture(GL_TEXTURE_2D, 0);
-}
-
-Engine::Texture2D::~Texture2D()
-{
-    PROFILE_FUNCTION();
-
-    glDeleteTextures(1, &m_RendererID);
 }
 
 std::shared_ptr<Engine::Texture2D> Engine::Texture2D::WhiteTexture()
 {
-    PROFILE_FUNCTION();
-
-    static std::shared_ptr<Texture2D> whiteTexture = std::make_shared<Texture2D>(
-        1, 1, GL_RGBA8, GL_RGBA, GL_UNSIGNED_BYTE, new unsigned char[4]{255, 255, 255, 255});
-    return whiteTexture;
-}
-
-void Engine::Texture2D::Bind(unsigned int slot) const
-{
-    PROFILE_FUNCTION();
-
-    if (m_Samples > 0)
+    switch (RendererProperty::GetInstance().GetAPI())
     {
-        LOG_ENGINE_WARN("Trying to bind a multisampled texture to slot: " + std::to_string(slot));
-        return;
+    case RendererAPI::OpenGL:
+        static std::shared_ptr<Texture2D> whiteTexture = std::make_shared<OpenGLTexture2D>(
+            1, 1, GL_RGBA8, GL_RGBA, GL_UNSIGNED_BYTE, new unsigned char[4]{255, 255, 255, 255});
+        return whiteTexture;
+    case RendererAPI::Vulkan:
+        LOG_ENGINE_ASSERT("Vulkan Texture2D is not implemented yet");
+        return nullptr;
+    default:
+        LOG_ENGINE_ASSERT("Unknown RendererAPI");
+        return nullptr;
     }
-
-    glActiveTexture(GL_TEXTURE0 + slot);
-    glBindTexture(GL_TEXTURE_2D, m_RendererID);
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
 }
-
-void Engine::Texture2D::Unbind() const
-{
-    PROFILE_FUNCTION();
-
-    glBindTexture(GL_TEXTURE_2D, 0);
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
-}
-
-void Engine::Texture2D::Resize(int width, int height)
-{
-    PROFILE_FUNCTION();
-
-    m_Width = width;
-    m_Height = height;
-
-    if (m_Samples == 0)
-    {
-        glBindTexture(GL_TEXTURE_2D, m_RendererID);
-        glTexImage2D(GL_TEXTURE_2D, 0, m_InternalFormat, m_Width, m_Height, 0, m_Format, m_Type, nullptr);
-        glBindTexture(GL_TEXTURE_2D, 0);
-    }
-    else
-    {
-        glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, m_RendererID);
-        glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, m_Samples, m_InternalFormat, m_Width, m_Height, GL_TRUE);
-        glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
-    }
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
-}
-
-void Engine::Texture2D::CreateTexture(int width, int height, unsigned int internalFormat, unsigned int format,
-                                      unsigned int type, void *data)
-{
-    PROFILE_FUNCTION();
-
-    glGenTextures(1, &m_RendererID);
-    glBindTexture(GL_TEXTURE_2D, m_RendererID);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, format, type, data);
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
-};
--- Engine/Platform/src/Renderers/Texture/Texture2D.hpp
+++ Engine/Platform/src/Renderers/Texture/Texture2D.hpp
@@ -8,13 +8,10 @@ namespace Engine
 class Texture2D : public Texture
 {
 public:
-    // Texture2D(int width, int height, void *data);
-    Texture2D(int width, int height, unsigned int internalFormat, unsigned int format, unsigned int type,
-              void *data = nullptr);
-    Texture2D(int width, int height, int samples);
-    Texture2D(const std::string &path);
-    virtual ~Texture2D() override;
-
+    static std::shared_ptr<Texture2D> Create(int width, int height, unsigned int internalFormat, unsigned int format,
+                                             unsigned int type, void *data = nullptr);
+    static std::shared_ptr<Texture2D> Create(int width, int height, int samples);
+    static std::shared_ptr<Texture2D> Create(const std::string &path);
     static std::shared_ptr<Texture2D> WhiteTexture();
 
     // Getters
@@ -22,18 +19,21 @@ public:
     int GetHeight() const { return m_Height; }
 
 public:
-    virtual void Bind(unsigned int slot) const override;
-    virtual void Unbind() const override;
-    void Resize(int width, int height);
+    virtual void Resize(int width, int height) = 0;
 
-private:
+protected:
     int m_Width, m_Height;
     unsigned int m_InternalFormat, m_Format, m_Type;
     int m_Samples;
 
-private:
-    void CreateTexture(int width, int height, unsigned int internalFormat, unsigned int format, unsigned int type,
-                       void *data);
+protected:
+    Texture2D() = default;
+    Texture2D(int width, int height, unsigned int internalFormat, unsigned int format, unsigned int type, int samples)
+        : Texture(), m_Width(width), m_Height(height), m_InternalFormat(internalFormat), m_Format(format), m_Type(type),
+          m_Samples(samples)
+    {
+    }
+    virtual ~Texture2D() override = default;
 };
 
 } // namespace Engine
