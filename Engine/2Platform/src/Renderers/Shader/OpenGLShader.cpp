1c6be31d648a2e3c4a08e4ca239d26440dca0b5f
zong4 <zzoonng@163.com>
2026-01-14 14:30:31 +0100

    detail log

--- Engine/2Platform/src/Renderers/Shader/OpenGLShader.cpp
+++ Engine/2Platform/src/Renderers/Shader/OpenGLShader.cpp
@@ -41,42 +41,42 @@ void Engine::OpenGLShader::SetUniformInt(const std::string &name, int value)
 {
     int location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniform1i(location, value);
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
+    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE) + " - Name: " + name);
 }
 
 void Engine::OpenGLShader::SetUniformUInt(const std::string &name, unsigned int value)
 {
     int location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniform1ui(location, value);
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
+    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE) + " - Name: " + name);
 }
 
 void Engine::OpenGLShader::SetUniformFloat(const std::string &name, float value)
 {
     int location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniform1f(location, value);
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
+    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE) + " - Name: " + name);
 }
 
 void Engine::OpenGLShader::SetUniformVec3(const std::string &name, const glm::vec3 &vector3)
 {
     int location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniform3fv(location, 1, &vector3[0]);
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
+    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE) + " - Name: " + name);
 }
 
 void Engine::OpenGLShader::SetUniformVec4(const std::string &name, const glm::vec4 &vector4)
 {
     int location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniform4fv(location, 1, &vector4[0]);
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
+    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE) + " - Name: " + name);
 }
 
 void Engine::OpenGLShader::SetUniformMat4(const std::string &name, const glm::mat4 &matrix4)
 {
     int location = glGetUniformLocation(m_RendererID, name.c_str());
     glUniformMatrix4fv(location, 1, GL_FALSE, &matrix4[0][0]);
-    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE));
+    RendererCommand::GetError(std::string(FUNCTION_SIGNATURE) + " - Name: " + name);
 }
 
 void Engine::OpenGLShader::Bind() const { glUseProgram(m_RendererID); }
