d53cc775e463c0a4ccf298388f79e3f329a60496
zong4 <zzoonng@163.com>
2026-01-19 13:58:00 +0100

    fix too far to culling

--- Engine/3Function/src/ECS/Components/CameraComponent.hpp
+++ Engine/3Function/src/ECS/Components/CameraComponent.hpp
@@ -23,7 +23,7 @@ struct CameraComponent
 
 public:
     CameraComponent(CameraType type = CameraType::Perspective, float width = 1280.0f, float height = 720.0f,
-                    float fov = 45.0f, float nearClip = 0.1f, float farClip = 100.0f,
+                    float fov = 45.0f, float nearClip = 0.1f, float farClip = 3000.0f,
                     const glm::vec4 &backgroundColor = glm::vec4(0.01f, 0.01f, 0.01f, 1.0f));
 
     // Getters
