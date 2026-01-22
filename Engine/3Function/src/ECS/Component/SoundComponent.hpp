e3d4fd58ab42ed7dc65b635a9cd4e7907d1fee98
zong4 <zzoonng@163.com>
2026-01-22 15:01:32 +0100

    add sound component

--- Engine/3Function/src/ECS/Component/SoundComponent.hpp
+++ Engine/3Function/src/ECS/Component/SoundComponent.hpp
@@ -2,19 +2,38 @@
 
 #include <Platform.hpp>
 
+class ma_engine;
+class ma_sound;
+
 namespace Engine
 {
 
+static ma_engine *GetSoundEngine();
+
 struct SoundComponent
 {
-    bool Looping;
-    float Volume;
+public:
+    SoundComponent(bool looping = false, float volume = 1.0f) : m_Looping(looping), m_Volume(volume) {}
+    ~SoundComponent();
+
+    // Getters
+    bool IsLooping() const { return m_Looping; }
+    float GetVolume() const { return m_Volume; }
+    ma_sound *GetSound() const { return m_Sound; }
+
+    // Setters
+    void SetLooping(bool loop);
+    void SetVolume(float volume);
+    void SetSound(const std::filesystem::path &filepath);
 
 public:
-    SoundComponent(bool looping = false, float volume = 1.0f) : Looping(looping), Volume(volume) {}
+    void Play();
+    void Stop();
 
-public:
-    void Play() {}
+private:
+    bool m_Looping;
+    float m_Volume;
+    ma_sound *m_Sound = nullptr;
 };
 
 } // namespace Engine
