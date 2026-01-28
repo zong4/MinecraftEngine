204fdd6acb1e81ed6d4310f0abb09a55b3b3724e
Zong <zzoonng@163.com>
2026-01-28 22:42:46 +0100

    fix

--- Engine/3Function/src/Application/EntryPoint.cpp
+++ Engine/3Function/src/Application/EntryPoint.cpp
@@ -5,22 +5,24 @@ int main()
     // Create Application
     std::unique_ptr<Engine::Application> app;
     {
-        Engine::Instrumentor::GetInstance().BeginSession(Engine::GetLogsDirectory() / "Profile-Init.json");
-        Engine::Logger::Init(Engine::GetLogsDirectory());
+        Engine::Instrumentor::GetInstance().BeginSession((Engine::GetLogsDirectory() / "Profile-Init.json").string());
+        Engine::Logger::Init(Engine::GetLogsDirectory().string());
         app = Engine::CreateApplication();
         Engine::Instrumentor::GetInstance().EndSession();
     }
 
     // Run Application
     {
-        Engine::Instrumentor::GetInstance().BeginSession(Engine::GetLogsDirectory() / "Profile-Runtime.json");
+        Engine::Instrumentor::GetInstance().BeginSession(
+            (Engine::GetLogsDirectory() / "Profile-Runtime.json").string());
         app->Run();
         Engine::Instrumentor::GetInstance().EndSession();
     }
 
     // Shutdown Application
     {
-        Engine::Instrumentor::GetInstance().BeginSession(Engine::GetLogsDirectory() / "Profile-Shutdown.json");
+        Engine::Instrumentor::GetInstance().BeginSession(
+            (Engine::GetLogsDirectory() / "Profile-Shutdown.json").string());
         app.reset();
         Engine::Instrumentor::GetInstance().EndSession();
     }
