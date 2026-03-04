10b17a9d0fa19df0e228121a7e45be3e04f9ac1f
zong4 <zzoonng@163.com>
2026-03-04 15:48:09 +0100

    add respawn in lua

---
+++ Editor/assets/Scripts/Respawn.lua
@@ -0,0 +1,28 @@
+local Script = {}
+
+local resetY = -1.0
+local spawnPosition = nil
+
+function Script:OnCreate()
+    local transform = self.Entity:GetTransform()
+    spawnPosition = {
+        x = transform.Position.x,
+        y = transform.Position.y,
+        z = transform.Position.z
+    }
+end
+
+function Script:OnUpdate(dt)
+    local transform = self.Entity:GetTransform()
+    if transform.Position.y < resetY then
+        transform.Position.x = spawnPosition.x
+        transform.Position.y = spawnPosition.y
+        transform.Position.z = spawnPosition.z
+        Debug.Log("Player reset to spawn position")
+    end
+end
+
+function Script:OnDestroy()
+end
+
+return Script
